119707553f6e1b45fd472e1a2e926c6ebf0d19fa
Xiao-Zhang-1163066 <xiao.zhang4@lincolnuni.ac.nz>
2026-05-10 16:38:39 +1200

    Switch to ts. Add shacn/ui

--- client/src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1 className="text-3xl font-bold text-center mt-10">Movie Mate</h1>;
-}
-
-export default App;
---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      {/* center the head */}
+      <h1 className="text-2xl">movie</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
